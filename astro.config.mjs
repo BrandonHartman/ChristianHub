87659d2b4cdb24c336703ad60b8381e81996da4e
Brandon Hartman <brandon@jokerbusinesssolutions.com>
2025-05-16 21:11:45 -0400

    Added Astro Island Content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://christianhub.netlify.app/"
-
+  site: "https://christianhub.netlify.app/",
+  integrations: [preact()]
 });
